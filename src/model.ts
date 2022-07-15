2ea2b181dc001fdde21627bcd1042e6c856322a0
Swifter <swifter1243@gmail.com>
2022-07-15 15:53:47 -0400

    reorganizing model stuff

--- src/model.ts
+++ src/model.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore-file no-explicit-any
-import { ColorType, eulerFromQuaternion, getSeconds, RMJson } from "./general.ts";
-import { RawKeyframesVec3 } from "./animation.ts";
+import { ColorType, eulerFromQuaternion, getSeconds, RMJson, rotatePoint, Vec3 } from "./general.ts";
+import { RawKeyframesVec3, toPointDef } from "./animation.ts";
 import { path, fs, blender } from "./deps.ts";
 
 export interface ModelCube {
@@ -76,6 +76,109 @@ export function cacheModel(filePath: string, process: () => ModelCube[], process
     }
 
     return outputData;
+}
+
+const blenderShrink = 9 / 10; // For whatever reason.. this needs to be multiplied to all of the scales to make things look proper... who knows man.
+let modelEnvCount = 0;
+let noYeet = true;
+
+enum MODEL_GROUP_TYPE {
+    ENVIRONMENT = 0
+}
+
+type ModelGroupConfig = {
+    anchor?: Vec3,
+    scale?: Vec3,
+    rotation?: Vec3,
+    type?: MODEL_GROUP_TYPE,
+    track?: string,
+    spawn?: boolean
+    id?: string,
+    lookup?: string
+    disappearWhenAbsent?: boolean
+}
+
+type GroupObjectTypes = EnvironmentGroup;
+
+export class EnvironmentGroup {
+    config: EnvironmentGroupType
+
+    constructor(config: EnvironmentGroupType = {}) {
+        this.config = config;
+    }
+}
+
+type EnvironmentGroupType = {
+    anchor?: Vec3
+    scale?: Vec3
+    rotation?: Vec3
+    id?: string
+    lookup?: string
+}
+
+export class ModelScene {
+    groups: ModelGroupConfig[] = []
+
+    constructor(config: GroupObjectTypes) {
+        this.pushGroup(config);
+        modelEnvCount++;
+    }
+
+    private pushGroup(config: GroupObjectTypes, group?: (config: ModelGroupConfig) => void) {
+        const primaryConfig = config.config as ModelGroupConfig;
+        if (group) group(primaryConfig);
+
+        if (config instanceof EnvironmentGroup) primaryConfig.type = MODEL_GROUP_TYPE.ENVIRONMENT;
+
+        this.groups.push(primaryConfig);
+    }
+
+    addPrimaryGroups(track: string | string[], config: GroupObjectTypes) {
+        const tracks = typeof track === "object" ? track : [track];
+        tracks.forEach(t => {
+            this.pushGroup(config, x => {
+                x.track = t;
+            })
+        })
+    }
+
+    assignObjects(track: string | string[], config: GroupObjectTypes, disappearWhenAbsent = true) {
+        const tracks = typeof track === "object" ? track : [track];
+        tracks.forEach(t => {
+            this.pushGroup(config, x => {
+                x.track = t;
+                x.disappearWhenAbsent = disappearWhenAbsent;
+            })
+        })
+    }
+}
+
+const modelScene = new ModelScene(new EnvironmentGroup({
+    anchor: [0, 0, 0]
+}))
+console.log(modelScene.groups);
+
+/**
+ * Used by ModelEnvironment to transform cube data to represent an environment object.
+ * @param objPos 
+ * @param objRot 
+ * @param objScale 
+ * @param anchor 
+ * @param scale 
+ * @returns 
+ */
+ export function applyAnchorAndScale(objPos: Vec3, objRot: Vec3, objScale: Vec3, anchor: Vec3, scale: Vec3) {
+    const offset = rotatePoint(objRot, objScale.map((x, i) => x * -anchor[i] * blenderShrink) as Vec3);
+    objPos = objPos.map((x, i) => x + offset[i]) as Vec3;
+    objScale = objScale.map((x, i) => x * scale[i] * blenderShrink) as Vec3;
+    return { pos: objPos, rot: objRot, scale: objScale };
+}
+
+function createYeetDef() {
+    if (noYeet === true) {
+        noYeet = false;
+        toPointDef([0, -69420, 0], "yeet");
+    }
 }
 
 export function getModelCubesFromCollada(filePath: string) {
