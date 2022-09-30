228f594eb04cd958234245b5a6143c4dc2f8a162
Swifter <swifter1243@gmail.com>
2022-09-30 00:05:51 -0400

    common class stuff for beatmap objects

--- src/object.ts
+++ src/object.ts
@@ -1,5 +1,7 @@
 // deno-lint-ignore-file
-import { copy, isEmptyObject, jsonGet, jsonPrune, jsonSet } from "./general.ts";
+import { Track } from "./animation.ts";
+import { activeDiffGet, info } from "./beatmap.ts";
+import { ColorType, copy, getJumps, isEmptyObject, jsonCheck, jsonGet, jsonPrune, jsonRemove, jsonSet, Vec3 } from "./general.ts";
 
 export class BaseObject {
     json: Record<string, unknown> = {
@@ -7,8 +9,8 @@ export class BaseObject {
         _type: 0
     }
 
-    get time() { return this.json._time as number }
-    get type() { return this.json._type as number }
+    get time() { return jsonGet(this, "json._time") }
+    get type() { return jsonGet(this, "json._type") }
     get customData() { return jsonGet(this.json, "_customData") }
 
     set time(value: number) { this.json._time = value }
@@ -24,5 +26,52 @@ export class BaseObject {
 }
 
 export class BaseGameplayObject extends BaseObject {
-    
+    get position() { return jsonGet(this, "json._customData._position") }
+    get rotation() { return jsonGet(this, "json._customData._rotation") }
+    get localRotation() { return jsonGet(this, "json._customData._localRotation") }
+    get NJS() {
+        if (jsonCheck(this, "json._customData._noteJumpMovementSpeed"))
+            return jsonGet(this, "json._customData._noteJumpMovementSpeed");
+        else return activeDiffGet().NJS;
+    }
+    get offset() {
+        if (jsonCheck(this, "json._customData._noteJumpStartBeatOffset"))
+            return jsonGet(this, "json._customData._noteJumpStartBeatOffset");
+        else return activeDiffGet().offset;
+    }
+    get halfJumpDur() { return getJumps(this.NJS, this.offset, info.BPM).halfDur }
+    get jumpDist() { return getJumps(this.NJS, this.offset, info.BPM).dist }
+    get life() { return this.halfJumpDur * 2 }
+    get lifeStart() { return this.time - this.life / 2 }
+    get fake() { return jsonGet(this, "json._customData._fake") }
+    get interactable() { return jsonGet(this, "json._customData._interactable") }
+    get track() { return new Track(jsonGet(this, "json._customData._track")) }
+    get color() { return jsonGet(this, "json._customData._color") }
+    get animation() { return jsonGet(this, "json._customData._animation") }
+
+    set position(value: Vec3) { jsonSet(this, "json._customData._position", value) }
+    set rotation(value: Vec3) { jsonSet(this, "json._customData._rotation", value) }
+    set localRotation(value: Vec3) { jsonSet(this, "json._customData._localRotation", value) }
+    set NJS(value: number) { jsonSet(this, "json._customData._noteJumpMovementSpeed", value) }
+    set offset(value: number) { jsonSet(this, "json._customData._noteJumpStartBeatOffset", value) }
+    set life(value: number) { 
+        if (value < 2) console.log("Warning: The lifespan of a note has a minimum of 2 beats.");
+        const defaultJumps = getJumps(this.NJS, 0, info.BPM);
+        this.offset = (value - (2 * defaultJumps.halfDur)) / 2;
+    }
+    set lifeStart(value: number) { this.time = value + this.life / 2 }
+    set fake(value: boolean) { jsonSet(this, "json._customData._fake", value) }
+    set interactable(value: boolean) { jsonSet(this, "json._customData._interactable", value) }
+    set color(value: ColorType) { jsonSet(this, "json._customData._color", value) }
+    set animation(value) { jsonSet(this, "json._customData._animation", value) }
+
+    get isGameplayModded() {
+        if (this.customData === undefined) return false;
+        const customData = copy(this.customData);
+        jsonRemove(customData, "_color");
+        jsonRemove(customData, "_disableSpawnEffect");
+        jsonRemove(customData, "_animation._color");
+        jsonPrune(customData);
+        return !isEmptyObject(customData);
+    }
 }
