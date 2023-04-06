dc7a3d40f66f88d6c73f4a4af2d03f35416121a5
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-04-06 13:45:46 -0400

    Finished animations

--- src/internals/animation.ts
+++ src/internals/animation.ts
@@ -13,19 +13,10 @@ import {
   simplifyArray,
 } from "../animation.ts";
 import { optimizeAnimation, OptimizeSettings } from "../anim_optimizer.ts";
-import { TJson } from "../beatmap.ts";
-import { ANIM } from "../constants.ts";
-import { Vec2, Vec3, Vec4 } from "../general.ts";
-import { FilterTypes } from "../types.ts";
-
-type BaseAnimationData<T> = FilterTypes<keyof T & string, KeyframesAny>;
 
 /** Bare minimum animation class. */
-export class BaseAnimation<
-  O,
-  T extends BaseAnimationData<O> = BaseAnimationData<O>,
-> {
-  properties: Partial<T> = {};
+export class BaseAnimation {
+  properties: Partial<Record<string, KeyframesAny>> = {};
 
   /**
    * The time in each keyframe is divided by the length.
@@ -33,7 +24,7 @@ export class BaseAnimation<
    */
   duration: number;
 
-  constructor(duration?: number, data?: BaseAnimation<O, T>["properties"]) {
+  constructor(duration?: number, data?: BaseAnimation["properties"]) {
     this.duration = duration ?? 1;
     this.properties = data ?? this.properties;
   }
@@ -43,10 +34,10 @@ export class BaseAnimation<
    * @param property The property to clear.
    * Leave undefined to clear everything in this animation.
    */
-  clear(property?: keyof T) {
+  clear(property?: string) {
     if (property !== undefined) delete this.properties[property];
     else {
-      Object.keys(this.properties).map((k) => k as keyof T).forEach((x) => {
+      Object.keys(this.properties).forEach((x) => {
         delete this.properties[x];
       });
     }
@@ -58,7 +49,7 @@ export class BaseAnimation<
    * @param value The value of the property.
    * @param process Whether the value should be processed. E.g. sort by time.
    */
-  set<K extends keyof T>(property: K, value: T[K] | undefined, process = true) {
+  set(property: string, value: KeyframesAny | undefined, process = true) {
     if (typeof value === "string" || !process) {
       this.properties[property] = value;
       return;
@@ -74,7 +65,7 @@ export class BaseAnimation<
         (a: KeyframeValues, b: KeyframeValues) =>
           new Keyframe(a).time - new Keyframe(b).time,
       ),
-    ) as T[K];
+    );
   }
 
   /**
@@ -84,18 +75,18 @@ export class BaseAnimation<
    * Time can be in length of animation or between 0 and 1 if negative.
    * Can be left undefined.
    */
-  get<K extends keyof T>(
-    property: K,
+  get(
+    property: string,
     time?: number,
-  ): T[K] | undefined {
+  ): KeyframesAny | undefined {
     const prop = this.properties[property];
     if (!prop) return undefined;
 
     if (time === undefined || typeof time === "string") {
-      return prop as T[K];
+      return prop;
     } else {
       time = this.convertTime(time);
-      return getValuesAtTime(property as string, prop, time) as T[K];
+      return getValuesAtTime(property as string, prop, time);
     }
   }
 
@@ -104,7 +95,7 @@ export class BaseAnimation<
    * @param property The property to add to.
    * @param value What keyframes to add.
    */
-  add<K extends keyof T>(property: K, value: T[K]) {
+  add(property: string, value: KeyframesAny) {
     if (typeof value === "string") {
       this.properties[property] = value;
       return;
@@ -126,7 +117,7 @@ export class BaseAnimation<
     const newValue = simplifyArray(
       concatArray.sort((a, b) => new Keyframe(a).time - new Keyframe(b).time),
     );
-    this.properties[property] = newValue as T[K];
+    this.properties[property] = newValue;
   }
 
   /**
@@ -135,13 +126,13 @@ export class BaseAnimation<
    * @param settings Options for the optimizer. Optional.
    */
   optimize(
-    property?: keyof T,
+    property?: string,
     settings: OptimizeSettings = new OptimizeSettings(),
   ) {
     if (property === undefined) {
-      const keys = Object.keys(this.properties) as (keyof T & string)[];
+      const keys = Object.keys(this.properties);
 
-      keys.forEach((key: keyof T & string) => {
+      keys.forEach((key) => {
         const val = this.properties[key];
         if (Array.isArray(val)) {
           const oldArray = this.get(key)!;
@@ -187,10 +178,8 @@ export class BaseAnimation<
 
 interface ObjectAnimationData {
   position: KeyframesVec3 | undefined;
-  offsetPosition: KeyframesVec3 | undefined;
   definitePosition: KeyframesVec3 | undefined;
   rotation: KeyframesVec3 | undefined;
-  offsetWorldRotation: KeyframesVec3 | undefined;
   localRotation: KeyframesVec3 | undefined;
   scale: KeyframesVec3 | undefined;
   dissolve: KeyframesLinear | undefined;
@@ -199,9 +188,77 @@ interface ObjectAnimationData {
   color: KeyframesVec4 | undefined;
 }
 
-class ObjectAnimation<
-  T extends ObjectAnimationData = ObjectAnimationData,
-> extends BaseAnimation<T> {}
+class ObjectAnimation extends BaseAnimation implements ObjectAnimationData {
+  /** Adds to the position of the object. */
+  get position() {
+    return this.get("offsetPosition") as KeyframesVec3;
+  }
+  set position(value: KeyframesVec3) {
+    this.set("offsetPosition", value);
+  }
+
+  /** Sets the absolute position of the object. */
+  get definitePosition() {
+    return this.get("definitePosition") as KeyframesVec3;
+  }
+  set definitePosition(value: KeyframesVec3) {
+    this.set("definitePosition", value);
+  }
+  /** Rotates the object around the world origin. */
+  get rotation() {
+    return this.get("offsetWorldRotation") as KeyframesVec3;
+  }
+  set rotation(value: KeyframesVec3) {
+    this.set("offsetWorldRotation", value);
+  }
+  /** Rotates the object around it's anchor point. */
+  get localRotation() {
+    return this.get("localRotation") as KeyframesVec3;
+  }
+  set localRotation(value: KeyframesVec3) {
+    this.set("localRotation", value);
+  }
+  /** Scales the object. */
+  get scale() {
+    return this.get("scale") as KeyframesVec3;
+  }
+  set scale(value: KeyframesVec3) {
+    this.set("scale", value);
+  }
+  /** Controls the dissolve shader on the object.
+   * 0 means invisible, 1 means visible.
+   */
+  get dissolve() {
+    return this.get("dissolve") as KeyframesLinear;
+  }
+  set dissolve(value: KeyframesLinear) {
+    this.set("dissolve", value);
+  }
+  /** Controls the color of the object. */
+  get color() {
+    return this.get("color") as KeyframesVec4;
+  }
+
+  set color(value: KeyframesVec4) {
+    this.set("color", value);
+  }
+  /** Controls whether the object is interactable.
+   * 0 = interactable, 1 means uninteractable.
+   */
+  get uninteractable() {
+    return this.get("uninteractable") as KeyframesLinear;
+  }
+  set uninteractable(value: KeyframesLinear) {
+    this.set("uninteractable", value);
+  }
+  /** Controls the time value for other animations. */
+  get time() {
+    return this.get("time") as KeyframesLinear;
+  }
+  set time(value: KeyframesLinear) {
+    this.set("time", value);
+  }
+}
 
 interface NoteAnimationData extends ObjectAnimationData {
   /** Controls the dissolve shader on the arrow.
@@ -211,119 +268,17 @@ interface NoteAnimationData extends ObjectAnimationData {
 }
 
 /** Animation specifically for note objects. */
-export class NoteAnimation extends ObjectAnimation<NoteAnimationData>
+export class NoteAnimation extends ObjectAnimation
   implements NoteAnimationData {
-  /** The position of the object in world space. For environment objects. */
-  get position(): KeyframesVec3 | undefined {
-    return this.get("position");
-  }
-  set position(value: KeyframesVec3 | undefined) {
-    this.set("position", value);
-  }
-
-  /** The rotation of the object in world space. For environment objects. */
-  get rotation() {
-    return this.get("rotation");
-  }
-  set rotation(value: KeyframesVec3 | undefined) {
-    this.set("rotation", value);
-  }
-
-  /** The position of the object relative to it's parent. For environment objects. */
-  get localPosition() {
-    return this.get("localPosition");
-  }
-  set localPosition(value: KeyframesVec3 | undefined) {
-    this.set("localPosition", value);
-  }
-  /** The rotation of the object relative to it's parent. For environment objects. */
-  get localRotation() {
-    return this.get("localRotation");
-  }
-  set localRotation(value: KeyframesVec3 | undefined) {
-    this.set("localRotation", value);
-  }
-  /** Adds to the position of the object. For gameplay objects. */
-  get offsetPosition() {
-    return this.get("offsetPosition");
-  }
-  set offsetPosition(value: KeyframesVec3 | undefined) {
-    this.set("offsetPosition", value);
-  }
-  /** Adds to the rotation of the object. For gameplay objects.  */
-  get offsetRotation() {
-    return this.get("offsetWorldRotation");
-  }
-  set offsetRotation(value: KeyframesVec3 | undefined) {
-    this.set("offsetWorldRotation", value);
-  }
-
-  /** Adds to the rotation of the object. For gameplay objects.  */
-  get offsetWorldRotation() {
-    return this.get("offsetWorldRotation");
-  }
-  set offsetWorldRotation(value: KeyframesVec3 | undefined) {
-    this.set("offsetWorldRotation", value);
-  }
-
-  /** Sets the absolute position of the object. For gameplay objects. */
-  get definitePosition() {
-    return this.get("definitePosition");
-  }
-  set definitePosition(value: KeyframesVec3 | undefined) {
-    this.set("definitePosition", value);
-  }
-  /** Sets the scale of the object. */
-  get scale() {
-    return this.get("scale");
-  }
-  set scale(value: KeyframesVec3 | undefined) {
-    this.set("scale", value);
-  }
-  /** Controls the dissolve shader on the object.
-   * 0 means invisible, 1 means visible.
-   * For gameplay objects.
-   */
-  get dissolve() {
-    return this.get("dissolve");
-  }
-  set dissolve(value: KeyframesLinear | undefined) {
-    this.set("dissolve", value);
-  }
   /** Controls the dissolve shader on the object.
    * 0 means invisible, 1 means visible.
    * For note objects.
    */
   get dissolveArrow() {
-    return this.get("dissolveArrow");
+    return this.get("dissolveArrow") as KeyframesLinear;
   }
   set dissolveArrow(value: KeyframesLinear | undefined) {
     this.set("dissolveArrow", value);
-  }
-  /** Controls the color of the object. */
-  get color() {
-    return this.get("color");
-  }
-  set color(value: KeyframesVec4 | undefined) {
-    this.set("color", value);
-  }
-  /** Controls whether the object is interactable.
-   * 0 = interactable, 1 means uninteractable.
-   */
-  get uninteractable() {
-    return this.get("uninteractable");
-  }
-
-  set uninteractable(value: KeyframesLinear | undefined) {
-    this.set("uninteractable", value);
-  }
-  /** Controls the time value for other animations. */
-  get time() {
-    return this.get("time");
-  }
-
-  set time(value: KeyframesLinear | undefined) {
-    this.set("time", value);
   }
 }
 
@@ -344,136 +299,147 @@ interface EnvironmentAnimationData {
 }
 
 /** Animation specifically for environment objects. */
-export class EnvironmentAnimation
-  extends BaseAnimation<EnvironmentAnimationData> {
-  set rotation(value: KeyframesVec3) {
+export class EnvironmentAnimation extends BaseAnimation {
+  /** The position of the object in world space. */
+  get position() {
+    return this.get("position") as KeyframesVec3;
+  }
+  set position(value: KeyframesVec3 | undefined) {
+    this.set("position", value);
+  }
+  /** The rotation of the object in world space. */
+  get rotation() {
+    return this.get("rotation") as KeyframesVec3;
+  }
+  set rotation(value: KeyframesVec3 | undefined) {
     this.set("rotation", value);
   }
-  set localPosition(value: KeyframesVec3) {
+  /** The position of the object relative to it's parent. */
+  get localPosition() {
+    return this.get("localPosition") as KeyframesVec3;
+  }
+  set localPosition(value: KeyframesVec3 | undefined) {
     this.set("localPosition", value);
   }
-  set localRotation(value: KeyframesVec3) {
+  /** The rotation of the object relative to it's parent. */
+  get localRotation() {
+    return this.get("localRotation") as KeyframesVec3;
+  }
+  set localRotation(value: KeyframesVec3 | undefined) {
     this.set("localRotation", value);
   }
-  set scale(value: KeyframesVec3) {
+  /** The scale of the object. */
+  get scale() {
+    return this.get("scale") as KeyframesVec3;
+  }
+  set scale(value: KeyframesVec3 | undefined) {
     this.set("scale", value);
   }
 }
 
 /** Animation that can apply to any object. */
-export class AbstractAnimation extends BaseAnimation<
-  EnvironmentAnimationData & NoteAnimationData & ObjectAnimationData
-> implements EnvironmentAnimationData, NoteAnimationData, ObjectAnimationData {
-  /** The position of the object in world space. For environment objects. */
+export class AbstractAnimation extends BaseAnimation
+  implements EnvironmentAnimationData, NoteAnimationData, ObjectAnimationData {
+  /** The position of the object in world space. */
   get position() {
-    return this.get("position");
+    return this.get("position") as KeyframesVec3;
   }
   set position(value: KeyframesVec3 | undefined) {
     this.set("position", value);
   }
-
-  /** The rotation of the object in world space. For environment objects. */
+  /** The rotation of the object in world space. */
   get rotation() {
-    return this.get("rotation");
+    return this.get("rotation") as KeyframesVec3;
   }
   set rotation(value: KeyframesVec3 | undefined) {
     this.set("rotation", value);
   }
-
-  /** The position of the object relative to it's parent. For environment objects. */
+  /** The position of the object relative to it's parent. */
   get localPosition() {
-    return this.get("localPosition");
+    return this.get("localPosition") as KeyframesVec3;
   }
   set localPosition(value: KeyframesVec3 | undefined) {
     this.set("localPosition", value);
   }
-  /** The rotation of the object relative to it's parent. For environment objects. */
+  /** The rotation of the object relative to it's parent. */
   get localRotation() {
-    return this.get("localRotation");
+    return this.get("localRotation") as KeyframesVec3;
   }
   set localRotation(value: KeyframesVec3 | undefined) {
     this.set("localRotation", value);
   }
-  /** Adds to the position of the object. For gameplay objects. */
-  get offsetPosition() {
-    return this.get("offsetPosition");
-  }
-  set offsetPosition(value: KeyframesVec3 | undefined) {
-    this.set("offsetPosition", value);
-  }
-  /** Adds to the rotation of the object. For gameplay objects.  */
-  get offsetRotation() {
-    return this.get("offsetWorldRotation");
-  }
-  set offsetRotation(value: KeyframesVec3 | undefined) {
-    this.set("offsetWorldRotation", value);
-  }
-
-  /** Adds to the rotation of the object. For gameplay objects.  */
-  get offsetWorldRotation() {
-    return this.get("offsetWorldRotation");
-  }
-  set offsetWorldRotation(value: KeyframesVec3 | undefined) {
-    this.set("offsetWorldRotation", value);
-  }
-
-  /** Sets the absolute position of the object. For gameplay objects. */
-  get definitePosition() {
-    return this.get("definitePosition");
-  }
-  set definitePosition(value: KeyframesVec3 | undefined) {
-    this.set("definitePosition", value);
-  }
-  /** Sets the scale of the object. */
+  /** The scale of the object. */
   get scale() {
-    return this.get("scale");
+    return this.get("scale") as KeyframesVec3;
   }
   set scale(value: KeyframesVec3 | undefined) {
     this.set("scale", value);
   }
-  /** Controls the dissolve shader on the object.
-   * 0 means invisible, 1 means visible.
-   * For gameplay objects.
-   */
-  get dissolve() {
-    return this.get("dissolve");
-  }
-  set dissolve(value: KeyframesLinear | undefined) {
-    this.set("dissolve", value);
-  }
+
   /** Controls the dissolve shader on the object.
    * 0 means invisible, 1 means visible.
    * For note objects.
    */
   get dissolveArrow() {
-    return this.get("dissolveArrow");
+    return this.get("dissolveArrow") as KeyframesLinear;
   }
   set dissolveArrow(value: KeyframesLinear | undefined) {
     this.set("dissolveArrow", value);
   }
+
+  /** Adds to the position of the object. */
+  get offsetPosition() {
+    return this.get("offsetPosition") as KeyframesVec3;
+  }
+  set offsetPposition(value: KeyframesVec3) {
+    this.set("offsetPosition", value);
+  }
+
+  /** Sets the absolute position of the object. */
+  get definitePosition() {
+    return this.get("definitePosition") as KeyframesVec3;
+  }
+  set definitePosition(value: KeyframesVec3) {
+    this.set("definitePosition", value);
+  }
+  /** Rotates the object around the world origin. */
+  get offsetWorldRotation() {
+    return this.get("offsetWorldRotation") as KeyframesVec3;
+  }
+  set offsetWorldRotation(value: KeyframesVec3) {
+    this.set("offsetWorldRotation", value);
+  }
+  /** Controls the dissolve shader on the object.
+   * 0 means invisible, 1 means visible.
+   */
+  get dissolve() {
+    return this.get("dissolve") as KeyframesLinear;
+  }
+  set dissolve(value: KeyframesLinear) {
+    this.set("dissolve", value);
+  }
   /** Controls the color of the object. */
   get color() {
-    return this.get("color");
+    return this.get("color") as KeyframesVec4;
   }
-  set color(value: KeyframesVec4 | undefined) {
+
+  set color(value: KeyframesVec4) {
     this.set("color", value);
   }
   /** Controls whether the object is interactable.
    * 0 = interactable, 1 means uninteractable.
    */
   get uninteractable() {
-    return this.get("uninteractable");
+    return this.get("uninteractable") as KeyframesLinear;
   }
-
-  set uninteractable(value: KeyframesLinear | undefined) {
+  set uninteractable(value: KeyframesLinear) {
     this.set("uninteractable", value);
   }
   /** Controls the time value for other animations. */
   get time() {
-    return this.get("time");
+    return this.get("time") as KeyframesLinear;
   }
-
-  set time(value: KeyframesLinear | undefined) {
+  set time(value: KeyframesLinear) {
     this.set("time", value);
   }
 }
