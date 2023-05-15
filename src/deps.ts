e0dfe9404b3bcd678fff10102906c0d067d34693
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-05-15 12:31:58 -0400

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,11 +1,10 @@
-export * as path from 'https://deno.land/std@0.181.0/path/mod.ts'
-export * as semver from 'https://deno.land/std@0.181.0/semver/mod.ts'
-export * as fs from 'https://deno.land/std@0.181.0/fs/mod.ts'
-export { compress } from 'https://deno.land/x/zip@v1.2.3/mod.ts'
+export * as path from 'https://deno.land/std@0.187.0/path/mod.ts'
+export * as semver from 'https://deno.land/std@0.187.0/semver/mod.ts'
+export * as fs from 'https://deno.land/std@0.187.0/fs/mod.ts'
+export { compress } from 'https://deno.land/x/zip@v1.2.5/mod.ts'
 export * as three from 'https://cdn.skypack.dev/three?dts'
 export * as adbDeno from 'https://deno.land/x/adb_deno@0.1.4/lib.ts'
 
 // Temporary measure
-export * as bsmap from 'https://raw.githubusercontent.com/Fernthedev/BeatSaber-Deno/feat/export-types/types/beatmap/mod.ts'
-
-//"https://deno.land/x/bsmap@1.3.2/types/beatmap/mod.ts";
+// export * as bsmap from 'https://raw.githubusercontent.com/Fernthedev/BeatSaber-Deno/feat/export-types/types/beatmap/mod.ts'
+export * as bsmap from "https://deno.land/x/bsmap@1.3.4/types/beatmap/mod.ts";
