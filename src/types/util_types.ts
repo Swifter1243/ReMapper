93db47a8bd75312dc67bb1a241435f2e2d84e707
Swifter <swifter1243@gmail.com>
2023-05-27 18:13:55 -0400

    return to old copy function o7

--- src/types/util_types.ts
+++ src/types/util_types.ts
@@ -1,38 +1,48 @@
 export type MapTypes<T, U, V> =
     & ExcludeTypes<T, U>
     & {
-    [K in keyof T]: T[K] extends U ? V : never
-}[keyof T]
+        [K in keyof T]: T[K] extends U ? V : never
+    }[keyof T]
+
 export type MapRecursiveTypes<T, U, V> =
     & ExcludeTypes<T, U>
     & {
-    [K in keyof T]: T[K] extends U ? V
-        : (T[K] extends object ? MapRecursiveTypes<T[K], U, V> : never)
-}[keyof T]
+        [K in keyof T]: T[K] extends U ? V
+            : (T[K] extends object ? MapRecursiveTypes<T[K], U, V> : never)
+    }[keyof T]
+
 export type FilterTypes<T, U> = Pick<
     T,
     {
         [K in keyof T]: T[K] extends U ? K : never
     }[keyof T]
 >
+
 export type ExcludeTypes<T, U> = Pick<
     T,
     {
         [K in keyof T]: T[K] extends U ? never : K
     }[keyof T]
 >
+
 // deno-lint-ignore ban-types
 type ExcludeFunctionPropertyNames<T> = ExcludeTypes<T, Function>
+
 // https://stackoverflow.com/questions/61272153/typescript-keyof-exclude-methods
 export type Fields<T> = ExcludeFunctionPropertyNames<T>
+
 export type OnlyNumbers<T> = FilterTypes<T, number>
+
 export type OnlyNumbersOptional<T> = FilterTypes<T, number | undefined>
+
 /** A type that can be used to prefer a tuple on an array of numbers. */
 export type NumberTuple = number[] | []
+
 export type ObjectFields<T extends { customData: V }, V = T['customData']> =
     & Omit<Fields<T>, 'customData'>
     & {
-    customData?: T['customData']
-}
+        customData?: T['customData']
+    }
+
 /** Type for Json data. */
 export type TJson = Record<string, unknown>
--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 /**
  * Copies @param obj with the new properties in @param overwrite
  * @param obj Original
@@ -20,5 +21,16 @@ export function copyWith<T extends Record<string | number | symbol, never>>(
  * @returns The copy
  */
 export function copy<T>(obj: T) {
-    return structuredClone(obj) as T
+    if (obj === null || typeof obj !== "object") { return obj; }
+
+    const newObj = Array.isArray(obj) ? [] : {};
+    const keys = Object.getOwnPropertyNames(obj);
+
+    keys.forEach(x => {
+        const value = copy((obj as any)[x]);
+        (newObj as any)[x] = value;
+    })
+
+    Object.setPrototypeOf(newObj, obj as any);
+    return newObj as T;
 }
