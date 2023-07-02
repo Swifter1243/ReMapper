c6fcefd8e2632de742bb1c973f147e344e2db0ef
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-07-02 13:10:04 -0400

    Use NPM for threejs

--- src/deps.ts
+++ src/deps.ts
@@ -1,13 +1,14 @@
-export * as path from 'https://deno.land/std@0.187.0/path/mod.ts'
-export * as semver from 'https://deno.land/std@0.187.0/semver/mod.ts'
-export * as fs from 'https://deno.land/std@0.187.0/fs/mod.ts'
+export * as path from "https://deno.land/std@0.187.0/path/mod.ts";
+export * as semver from "https://deno.land/std@0.187.0/semver/mod.ts";
+export * as fs from "https://deno.land/std@0.187.0/fs/mod.ts";
 export * as asserts from "https://deno.land/std@0.189.0/testing/asserts.ts";
 
-export { compress } from 'https://deno.land/x/zip@v1.2.5/mod.ts'
-export * as adbDeno from 'https://deno.land/x/adb_deno@0.1.4/lib.ts'
+export { compress } from "https://deno.land/x/zip@v1.2.5/mod.ts";
+export * as adbDeno from "https://deno.land/x/adb_deno@0.1.4/lib.ts";
 
-export * as three from 'https://cdn.skypack.dev/three?dts'
+// @deno-types="npm:@types/three@*"
+export * as three from "npm:three@^0.154";
 
 // Temporary measure
 // export * as bsmap from 'https://raw.githubusercontent.com/Fernthedev/BeatSaber-Deno/feat/export-types/types/beatmap/mod.ts'
-export * as bsmap from 'https://deno.land/x/bsmap@1.3.4/types/beatmap/mod.ts'
+export * as bsmap from "https://deno.land/x/bsmap@1.3.4/types/beatmap/mod.ts";
