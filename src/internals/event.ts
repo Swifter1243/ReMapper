b26a0ffb8986a744a2cd1aaa6ebfdbf18a411dd4
Swifter <swifter1243@gmail.com>
2023-11-16 08:24:23 -0500

    unfinished commit so I can work on it at school

--- src/internals/event.ts
+++ src/internals/event.ts
@@ -1,6 +1,12 @@
 import { EventGroup } from '../data/constants.ts'
 import { bsmap } from '../deps.ts'
-import { InverseRotationAction, RotationAction, copy, getActiveDiff, jsonPrune } from '../mod.ts'
+import {
+    copy,
+    getActiveDiff,
+    InverseRotationAction,
+    jsonPrune,
+    RotationAction,
+} from '../mod.ts'
 import { ObjectFields, SubclassExclusiveProps } from '../types/util_types.ts'
 import { BaseObject, getCDProp } from './object.ts'
 
@@ -82,7 +88,8 @@ export class RotationEvent
     toJson(v3: true, prune?: boolean): bsmap.v3.IRotationEvent
     toJson(v3: false, prune?: boolean): bsmap.v2.IEventLaneRotation
     toJson(
-        v3: boolean, prune = true
+        v3: boolean,
+        prune = true,
     ): bsmap.v2.IEventLaneRotation | bsmap.v3.IRotationEvent {
         if (v3) {
             const output = {
@@ -182,7 +189,10 @@ export class BoostEvent
 
     toJson(v3: true, prune?: boolean): bsmap.v3.IColorBoostEvent
     toJson(v3: false, prune?: boolean): bsmap.v2.IEvent
-    toJson(v3 = true, prune = true): bsmap.v2.IEvent | bsmap.v3.IColorBoostEvent {
+    toJson(
+        v3 = true,
+        prune = true,
+    ): bsmap.v2.IEvent | bsmap.v3.IColorBoostEvent {
         if (v3) {
             const output = {
                 b: this.time,
@@ -200,5 +210,157 @@ export class BoostEvent
             _customData: this.customData,
         } satisfies bsmap.v2.IEvent
         return prune ? jsonPrune(output) : output
+    }
+}
+
+/*
+- V2 Custom Event
+- V2 Basic Event
+
+- V3 Custom Event
+- V3 Basic Event
+- V3 Event
+*/
+
+// V3 Basic Event -> V3 Event
+
+type V2BPM = bsmap.v2.IBPMChange | bsmap.v2.IBPMChangeOld | bsmap.v2.IEvent
+type V3BPM = bsmap.v3.IBPMChange | bsmap.v3.IBPMEvent
+
+export abstract class BPMEvent<
+    TV2 extends V2BPM,
+    TV3 extends V3BPM,
+> extends BaseObject<TV2, TV3> {
+    push(
+        clone = true,
+    ) {
+        getActiveDiff().bpmEvents.push(clone ? copy(this) : this)
+        return this
+    }
+}
+
+export class OfficialBPMEvent extends BPMEvent<
+    bsmap.v2.IEvent,
+    bsmap.v3.IBPMEvent
+> {
+    constructor(obj: Partial<ObjectFields<OfficialBPMEvent>>) {
+        super(obj)
+        this.bpm = obj.bpm ?? 0
+    }
+
+    bpm: number
+
+    fromBasicEvent(json: bsmap.v3.IBasicEvent) {
+        this.bpm = json.et
+        return this
+    }
+
+    fromJson(json: bsmap.v3.IBPMEvent, v3: true): this
+    fromJson(json: bsmap.v2.IEvent, v3: false): this
+    fromJson(
+        json: bsmap.v2.IEvent | bsmap.v3.IBPMEvent,
+        v3: boolean,
+    ): this {
+        type Params = SubclassExclusiveProps<
+            OfficialBPMEvent,
+            BPMEvent<
+                bsmap.v2.IEvent,
+                bsmap.v3.IBPMEvent
+            >
+        >
+
+        if (v3) {
+            const obj = json as bsmap.v3.IBPMEvent
+
+            const params = {
+                bpm: obj.m,
+            } as Params
+
+            Object.assign(this, params)
+            return super.fromJson(obj, true)
+        } else {
+            const obj = json as bsmap.v2.IEvent
+
+            const params = {
+                bpm: obj._floatValue,
+            } as Params
+
+            Object.assign(this, params)
+            return super.fromJson(obj, false)
+        }
+    }
+
+    toJson(v3: true, prune?: boolean): bsmap.v3.IBPMEvent
+    toJson(v3: false, prune?: boolean): bsmap.v2.IEvent
+    toJson(
+        v3 = true,
+        prune = true,
+    ): bsmap.v2.IEvent | bsmap.v3.IBPMEvent {
+        if (v3) {
+            const output = {
+                b: this.time,
+                m: this.bpm,
+                customData: this.customData,
+            } satisfies bsmap.v3.IBPMEvent
+            return prune ? jsonPrune(output) : output
+        }
+
+        const output = {
+            _time: this.time,
+            _floatValue: this.bpm,
+            _type: 100,
+            _value: 0,
+            _customData: this.customData,
+        } satisfies bsmap.v2.IEvent
+        return prune ? jsonPrune(output) : output
+    }
+}
+
+export class CommunityBPMEvent extends BPMEvent<
+    bsmap.v2.IBPMChange | bsmap.v2.IBPMChangeOld,
+    bsmap.v3.IBPMChange
+> {
+    constructor(obj: Partial<ObjectFields<CommunityBPMEvent>>) {
+        super(obj)
+        this.bpm = obj.bpm ?? 0
+        this.mediocreMapper = obj.mediocreMapper ?? false
+        this.beatsPerBar = obj.beatsPerBar ?? 4
+        this.metronomeOffset = obj.metronomeOffset ?? 0
+    }
+
+    bpm: number
+    mediocreMapper: boolean
+    beatsPerBar: number
+    metronomeOffset: number
+
+    toJson(v3: true, prune?: boolean): bsmap.v3.IBPMChange
+    toJson(
+        v3: false,
+        prune?: boolean,
+    ): bsmap.v2.IBPMChange | bsmap.v2.IBPMChangeOld
+    toJson(
+        v3 = true,
+        prune = true,
+    ): bsmap.v2.IBPMChange | bsmap.v2.IBPMChangeOld | bsmap.v3.IBPMChange {
+        if (v3) {
+            const output = {
+                b: this.time,
+                m: this.bpm,
+                o: this.metronomeOffset,
+                p: this.beatsPerBar,
+            } satisfies bsmap.v3.IBPMChange
+            return prune ? jsonPrune(output) : output
+        }
+
+        if (this.mediocreMapper) {
+            const output = {
+                _time: this.time,
+                _bpm: this.bpm,
+                _BPM: undefined as never,
+                _beatsPerBar: this.beatsPerBar,
+                _metronomeOffset: this.metronomeOffset,
+            } satisfies bsmap.v2.IBPMChangeOld
+            return prune ? jsonPrune(output) : output
+        }
     }
 }
