a603146aadec22207405aad2d1286963f2ea3211
Swifter <swifter1243@gmail.com>
2024-02-03 02:28:23 -0500

    reorganize lolol

--- src/utils/array_utils.ts
+++ src/utils/array_utils.ts
@@ -73,20 +73,6 @@ export function arraySubtract<T extends [] | number[]>(
 }
 
 /**
- * Interpolate to find an array between 2 arrays of the same length.
- * @param start Start array.
- * @param end End array.
- * @param fraction Value to find in between start and end.
- * @param easing Optional easing.
- */
-export const arrayLerp = <T extends [] | number[]>(
-    start: T,
-    end: { [K in keyof T]: number },
-    fraction: number,
-    easing?: EASE,
-) => start.map((x, i) => lerp(x, end[i], fraction, easing)) as T
-
-/**
  * Multiply an array either by a number or another array.
  * @param arr Input array.
  * @param value Can be a number or an array.
@@ -113,6 +99,20 @@ export function arrayDivide<T extends [] | number[]>(
         return arr.map((x) => x / value) as unknown as T
     } else return arr.map((x, i) => x / value[i]) as unknown as T
 }
+
+/**
+ * Interpolate to find an array between 2 arrays of the same length.
+ * @param start Start array.
+ * @param end End array.
+ * @param fraction Value to find in between start and end.
+ * @param easing Optional easing.
+ */
+export const arrayLerp = <T extends [] | number[]>(
+    start: T,
+    end: { [K in keyof T]: number },
+    fraction: number,
+    easing?: EASE,
+) => start.map((x, i) => lerp(x, end[i], fraction, easing)) as T
 
 /**
  * Check if 2 arrays are equal to each other.
