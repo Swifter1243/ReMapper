cb438147eeffea27fe948dd07919fb2d80458b3a
Swifter <swifter1243@gmail.com>
2024-07-21 21:54:38 -0400

    data import/export/jsdoc cleanup

--- src/data/constants/math.ts
+++ src/data/constants/math.ts
@@ -1,1 +1,2 @@
+/** very smol number */
 export const EPSILON = 1e-3
---
+++ src/data/mod.ts
@@ -0,0 +1,6 @@
+export * from './constants/mod.ts'
+export * from './active_cache.ts'
+export * from './active_difficulty.ts'
+export * from './active_info.ts'
+export * from './settings.ts'
+export * from './working_directory.ts'
