65d0fd0dccacf506c9d81b77b1b2945f953a7b37
Swifter <swifter1243@gmail.com>
2024-07-25 00:03:38 -0400

    idk why .object turned to .json

--- src/utils/rm_cache/location.ts
+++ src/utils/rm_cache/location.ts
@@ -1,7 +1,7 @@
 import { getWorkingDirectory } from '../../data/working_directory.ts'
 import { path } from '../../deps.ts'
 
-/** Get the path of RM_Cache.object */
+/** Get the path of RM_Cache.json */
 export function getCacheLocation() {
-    return path.join(getWorkingDirectory(), 'RM_Cache.object')
+    return path.join(getWorkingDirectory(), 'RM_Cache.json')
 }
