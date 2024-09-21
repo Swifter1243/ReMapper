cd936214bd3ea3c4675118247a491c75b387def8
Swifter <swifter1243@gmail.com>
2024-09-21 12:08:26 -0400

    prefab instance accessors

--- src/utils/bundle/prefab_instance.ts
+++ src/utils/bundle/prefab_instance.ts
@@ -1,19 +1,56 @@
 import {destroyPrefab} from "../../builder_functions/beatmap/object/custom_event/vivify.ts";
 
 import {InstantiatePrefab} from "../../internals/beatmap/object/custom_event/vivify/instantiate_prefab.ts";
+import {Vec3} from "../../types/math/vector.ts";
 
 /** An instance of a prefab. */
 export class PrefabInstance {
     /** The id/track of this instance. */
     readonly id: string
+    /** The event used to push this instance. */
+    readonly event: InstantiatePrefab
     /** The track of this instance. Equivalent to id. */
     get track() {
         return this.id
     }
     /** Whether this instance has been destroyed. */
     destroyed = false
-    /** The event used to push this instance. */
-    readonly event: InstantiatePrefab
+
+    /** The initial position of this prefab. */
+    get position() {
+        return this.event.position
+    }
+    set position(position: Vec3 | undefined) {
+        this.event.position = position
+    }
+    /** The initial local position of this prefab. */
+    get localPosition() {
+        return this.event.localPosition
+    }
+    set localPosition(localPosition: Vec3 | undefined) {
+        this.event.localPosition = localPosition
+    }
+    /** The initial rotation of this prefab. */
+    get rotation() {
+        return this.event.rotation
+    }
+    set rotation(rotation: Vec3 | undefined) {
+        this.event.rotation = rotation
+    }
+    /** The initial local rotation of this prefab. */
+    get localRotation() {
+        return this.event.position
+    }
+    set localRotation(localRotation: Vec3 | undefined) {
+        this.event.localRotation = localRotation
+    }
+    /** The initial scale of this prefab. */
+    get scale() {
+        return this.event.scale
+    }
+    set scale(scale: Vec3 | undefined) {
+        this.event.scale = scale
+    }
 
     constructor(id: string, event: InstantiatePrefab) {
         this.id = id
