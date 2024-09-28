1651139dd7e17486b40756ba1ca6330bee831e06
Swifter <swifter1243@gmail.com>
2024-09-28 00:54:28 -0400

    update SETTINGS_PRESET

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,40 +1,56 @@
+import { SettingsSetter } from '../types/beatmap/info/settings_setter.ts'
+
 /** Setting presets. You would set these equal to the "rawSettings" property on a difficulty. */
 export const SETTINGS_PRESET = {
+    /** Nothing on the settings setter. */
+    NONE: {
+        modifiers: {},
+        colors: {},
+        environments: {},
+        graphics: {},
+        playerOptions: {},
+        chroma: {}
+    },
     /** Settings which are good for maps that need chroma and noodle. */
     MODCHART_SETTINGS: {
-        _playerOptions: {
-            _environmentEffectsFilterExpertPlusPreset: 'AllEffects',
-            _environmentEffectsFilterDefaultPreset: 'AllEffects',
+        playerOptions: {
+            environmentEffectsFilterExpertPlusPreset: 'AllEffects',
+            environmentEffectsFilterDefaultPreset: 'AllEffects',
         },
-        _graphics: {
-            _mainEffectGraphicsSettings: 1,
-            _maxShockwaveParticles: 0,
-            _screenDisplacementEffectsEnabled: true,
+        graphics: {
+            bloomGraphicsSettings: 'On',
+            maxShockwaveParticles: 0,
+            screenDisplacementEffectsEnabled: true,
         },
-        _chroma: {
-            _disableEnvironmentEnhancements: false,
-            _disableChromaEvents: false,
-            _forceZenModeWalls: true,
+        chroma: {
+            disableEnvironmentEnhancements: false,
+            disableChromaEvents: false,
+            forceZenModeWalls: true,
         },
-        _modifiers: {
-            _enabledObstacleType: 'All',
+        modifiers: {
+            enabledObstacleType: 'All',
         },
+        colors: {},
+        environments: {},
     },
     /** Settings which are good for maps that have chroma environment stuff. */
     CHROMA_SETTINGS: {
-        _playerOptions: {
-            _environmentEffectsFilterExpertPlusPreset: 'AllEffects',
-            _environmentEffectsFilterDefaultPreset: 'AllEffects',
+        playerOptions: {
+            environmentEffectsFilterExpertPlusPreset: 'AllEffects',
+            environmentEffectsFilterDefaultPreset: 'AllEffects',
         },
-        _graphics: {
-            _mainEffectGraphicsSettings: 1,
+        graphics: {
+            bloomGraphicsSettings: 'On',
         },
-        _chroma: {
-            _disableEnvironmentEnhancements: false,
-            _disableChromaEvents: false,
+        chroma: {
+            disableEnvironmentEnhancements: false,
+            disableChromaEvents: false,
         },
+        environments: {},
+        colors: {},
+        modifiers: {},
     },
-}
+} satisfies Record<string, SettingsSetter>
 
 /** The track used to scale notes to `[1,1,1]` initially if enabled in the ReMapper settings. */
-export const DEFAULT_SCALED_TRACK = "RM_forceDefaultScale"
+export const DEFAULT_SCALED_TRACK = 'RM_forceDefaultScale'
