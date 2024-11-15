77d752ae5b1da64c9a945e63a8aa55c3d8071460
Swifter <swifter1243@gmail.com>
2024-11-15 05:31:48 -0500

    allow constructing workspace with no parameters

--- src/builder_functions/rm_workspace.ts
+++ src/builder_functions/rm_workspace.ts
@@ -3,11 +3,13 @@ import {createInfo} from "./beatmap/info.ts";
 import {ReMapperWorkspace} from "../internals/rm_workspace.ts";
 import {WorkspaceInitialization} from "../types/remapper/rm_workspace.ts";
 
-export async function createWorkspace(initialization: WorkspaceInitialization) {
-    const infoPath = path.join(initialization.infoPath ?? Deno.cwd(), 'Info.dat')
+export async function createWorkspace(): Promise<ReMapperWorkspace>
+export async function createWorkspace(initialization: WorkspaceInitialization): Promise<ReMapperWorkspace>
+export async function createWorkspace(initialization?: WorkspaceInitialization) {
+    const infoPath = path.join(initialization?.infoPath ?? Deno.cwd(), 'Info.dat')
     const info = await loadInfo(infoPath)
     const directory = path.dirname(infoPath)
-    return new ReMapperWorkspace(info, directory, initialization.bundleInfo)
+    return new ReMapperWorkspace(info, directory, initialization?.bundleInfo)
 }
 
 async function loadInfo(path: string) {
