d0c892154f78e004cd9e8faac0993079d8b99131
Swifter <swifter1243@gmail.com>
2024-11-25 21:13:11 -0500

    runtime modifier shit is now recursive

--- src/types/animation/points/runtime/structure.ts
+++ src/types/animation/points/runtime/structure.ts
@@ -1,4 +1,5 @@
 import { PointModifier } from '../components.ts'
+import { DecreaseNumber } from '../../../util/generate.ts'
 
 /** Values for runtime points.
  * [[...], [...], [...]] where [...] is [...x, time]
@@ -8,21 +9,12 @@ export type RuntimePointValues<
     T extends number[],
     R extends string,
 > =
-    | [...T, ...Modifier<T, R>[]]
-    | [R, ...Modifier<T, R>[]]
+    | [...T, ...RecursiveModifier<T, R>[]]
+    | [R, ...RecursiveModifier<T, R>[]]
 
-type Modifier<T extends number[], R extends string> =
-    | [...T, ...Modifier2<T, R>[], PointModifier]
-    | [R, ...Modifier2<T, R>[], PointModifier]
-
-type Modifier2<T extends number[], R extends string> =
-    | [...T, ...Modifier3<T, R>[], PointModifier]
-    | [R, ...Modifier3<T, R>[], PointModifier]
-
-type Modifier3<T extends number[], R extends string> =
-    | [...T, ...Modifier4<T, R>[], PointModifier]
-    | [R, ...Modifier4<T, R>[], PointModifier]
-
-type Modifier4<T extends number[], R extends string> =
-    | [...T, PointModifier]
-    | [R, PointModifier]
+// what the fuck
+type RecursiveModifier<T extends number[], R extends string, Depth extends number = 20> = Depth extends 0
+    ? [...T, PointModifier] | [R, PointModifier]
+    :
+        | [...T, ...RecursiveModifier<T, R, DecreaseNumber<Depth>>[], PointModifier]
+        | [R, ...RecursiveModifier<T, R, DecreaseNumber<Depth>>[], PointModifier]
--- src/types/util/generate.ts
+++ src/types/util/generate.ts
@@ -1,2 +1,6 @@
 /** Given a number N, generate a union of whole numbers from 0 to N */
 export type RangeOf<N extends number, R extends unknown[] = []> = R['length'] extends N ? R[number] : RangeOf<N, [...R, R['length']]>;
+
+/** Decrease a number by 1. */
+export type DecreaseNumber<N extends number, T extends unknown[] = []> = [...T, unknown]['length'] extends N ? T['length']
+    : DecreaseNumber<N, [...T, unknown]>
