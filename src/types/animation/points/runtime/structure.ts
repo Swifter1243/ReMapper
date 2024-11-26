87d87abd420d71c01ac85cab79edd3950f7573f9
Swifter <swifter1243@gmail.com>
2024-11-25 21:05:53 -0500

    hatred fills my heart

--- src/types/animation/points/runtime/structure.ts
+++ src/types/animation/points/runtime/structure.ts
@@ -1,4 +1,4 @@
-import { PointModifier } from "../components.ts";
+import { PointModifier } from '../components.ts'
 
 /** Values for runtime points.
  * [[...], [...], [...]] where [...] is [...x, time]
@@ -12,5 +12,17 @@ export type RuntimePointValues<
     | [R, ...Modifier<T, R>[]]
 
 type Modifier<T extends number[], R extends string> =
+    | [...T, ...Modifier2<T, R>[], PointModifier]
+    | [R, ...Modifier2<T, R>[], PointModifier]
+
+type Modifier2<T extends number[], R extends string> =
+    | [...T, ...Modifier3<T, R>[], PointModifier]
+    | [R, ...Modifier3<T, R>[], PointModifier]
+
+type Modifier3<T extends number[], R extends string> =
+    | [...T, ...Modifier4<T, R>[], PointModifier]
+    | [R, ...Modifier4<T, R>[], PointModifier]
+
+type Modifier4<T extends number[], R extends string> =
     | [...T, PointModifier]
     | [R, PointModifier]
