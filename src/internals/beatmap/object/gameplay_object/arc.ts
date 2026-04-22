e87bf7acd87b991555f1d58b84920a6502216d23
air <61111054+quiltedhills@users.noreply.github.com>
2026-04-22 05:42:49 +0300

    Fix arc tail default cut direction
    Very similar to bf54ff4 except it's more just arcs being weird than us messing up defaults :p

--- src/internals/beatmap/object/gameplay_object/arc.ts
+++ src/internals/beatmap/object/gameplay_object/arc.ts
@@ -39,7 +39,7 @@ export class Arc extends BaseSliderObject<bsmap.v3.IArc> {
     disableNoteGravity?: boolean
 
     static override defaults: GameplayObjectDefaults<Arc> = {
-        tailCutDirection: NoteCut.DOWN,
+        tailCutDirection: NoteCut.UP,
         headLength: 0,
         tailLength: 0,
         anchorMode: AnchorMode.STRAIGHT,
